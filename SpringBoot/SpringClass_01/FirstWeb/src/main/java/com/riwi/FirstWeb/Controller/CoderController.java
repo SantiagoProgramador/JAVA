1d0d7d9fd8c1f28f4e4b2047c4cde78122a3aec8
RiwiWork <pepe@gmail.com>
2024-04-18 09:30:03 -0500

    First web page with spring complete :D

--- SpringBoot/SpringClass_01/FirstWeb/src/main/java/com/riwi/FirstWeb/Controller/CoderController.java
+++ SpringBoot/SpringClass_01/FirstWeb/src/main/java/com/riwi/FirstWeb/Controller/CoderController.java
@@ -3,6 +3,7 @@ package com.riwi.FirstWeb.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.riwi.FirstWeb.Entity.Coder;
 import com.riwi.FirstWeb.Services.CoderService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/")
@@ -18,15 +23,50 @@ public class CoderController {
   private CoderService coderService;
 
   @GetMapping
-  public String showCoders(Model model) {
-    List<Coder> list = this.coderService.findAll();
+  public String showCoders(Model model, @RequestParam(defaultValue = "1") int page,
+      @RequestParam(defaultValue = "3") int size) {
+    Page<Coder> list = this.coderService.findPaginated(page - 1, size);
     model.addAttribute("Coderlist", list);
+    model.addAttribute("CurrentPage", page);
+    model.addAttribute("TotalPage", list.getTotalPages());
+
     return "Coders";
   }
 
-  // @GetMapping
-  // public String addCoder(Model model) {
-  // model.addAttribute("Coder", new Coder());
-  // return "CoderForm";
-  // }
+  @GetMapping("CoderForm")
+  public String addCoderForm(Model model) {
+    Coder coder = new Coder();
+    model.addAttribute("coder", coder);
+    model.addAttribute("action", "path");
+    return "CoderForm";
+  }
+
+  @PostMapping("path")
+  public String coderToAdd(@ModelAttribute Coder coder) {
+    this.coderService.addCoder(coder);
+
+    return "redirect:/";
+  }
+
+  @GetMapping("update/{id}")
+  public String showUpdateForm(@PathVariable Long id, Model model) {
+    Coder coder = this.coderService.findCoderById(id);
+    model.addAttribute("coder", coder);
+    model.addAttribute("action", "/edit/" + id);
+    return "CoderForm";
+  }
+
+  @PostMapping("/edit/{id}")
+  public String coderToUpdate(@PathVariable Long id, @ModelAttribute Coder coder) {
+    this.coderService.updateCoder(id, coder);
+
+    return "redirect:/";
+  }
+
+  @GetMapping("delete/{id}")
+  public String coderToDelete(@PathVariable Long id, Model model) {
+    this.coderService.deleteCoder(id);
+    return "redirect:/";
+  }
+
 }
--- SpringBoot/SpringClass_01/FirstWeb/src/main/java/com/riwi/FirstWeb/Services/CoderService.java
+++ SpringBoot/SpringClass_01/FirstWeb/src/main/java/com/riwi/FirstWeb/Services/CoderService.java
@@ -3,6 +3,9 @@ package com.riwi.FirstWeb.Services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.riwi.FirstWeb.Entity.Coder;
@@ -16,4 +19,43 @@ public class CoderService {
   public List<Coder> findAll() {
     return this.coderRepository.findAll();
   }
+
+  public Coder addCoder(Coder coder) {
+    return this.coderRepository.save(coder);
+  }
+
+  public Coder updateCoder(Long id, Coder coder) {
+    Coder coderUpdate = findCoderById(id);
+
+    if (coderUpdate == null) {
+      return null;
+    }
+    coderUpdate = coder;
+    return this.coderRepository.save(coderUpdate);
+  }
+
+  public Coder deleteCoder(Long id) {
+    Coder coderDelete = findCoderById(id);
+
+    if (coderDelete == null) {
+      return null;
+    }
+    this.coderRepository.delete(coderDelete);
+    return coderDelete;
+  }
+
+  public Coder findCoderById(Long id) {
+    return this.coderRepository.findById(id).orElse(null);
+  }
+
+  public Page<Coder> findPaginated(int page, int size) {
+    if (page < 0) {
+      page = 1;
+    }
+
+    Pageable pageable = PageRequest.of(page, size);
+
+    return this.coderRepository.findAll(pageable);
+  }
+
 }
