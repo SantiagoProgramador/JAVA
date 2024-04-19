986ad02d4c7da980a089ae4730c9e05d60f0b37a
SantiagoProgramador <andresx1277@gmail.com>
2024-04-19 13:47:55 -0500

    Update TaskService.java

--- SpringBoot/dailyTasks/src/main/java/com/dailyTasks/dailyTasks/Service/TaskService.java
+++ SpringBoot/dailyTasks/src/main/java/com/dailyTasks/dailyTasks/Service/TaskService.java
@@ -15,13 +15,45 @@ public class TaskService {
   private TaskRepository taskRepository;
 
   public Task findTaskById(Long id) {
-
     return this.taskRepository.findById(id).orElse(null);
   }
 
-  public List<Task> filterTasks(String title) {
-
+  public List<Task> findAll() {
     return this.taskRepository.findAll();
   }
 
+  public Task addTask(Task task) {
+    return this.taskRepository.save(task);
+  }
+
+  public Task updateTask(Long id, Task task) {
+    Task taskUpdate = findTaskById(id);
+
+    if (taskUpdate == null) {
+      return null;
+    }
+    taskUpdate = task;
+    return this.taskRepository.save(taskUpdate);
+  }
+
+  public Task deleteTask(Long id) {
+    Task taskDelete = findTaskById(id);
+
+    if (taskDelete == null) {
+      return null;
+    }
+    this.taskRepository.delete(taskDelete);
+    return taskDelete;
+  }
+  
+  public Page<Task> findPaginated(int page, int size) {
+    if (page < 0) {
+      page = 1;
+    }
+
+    Pageable pageable = PageRequest.of(page, size);
+
+    return this.taskRepository.findAll(pageable);
+  }
 }
+
